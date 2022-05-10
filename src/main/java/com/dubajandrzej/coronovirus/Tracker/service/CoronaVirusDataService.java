fc96d2199174d8dbfe5a5b01b89bfaf761f1e975
a-dubaj <dubaj.andrzej@gmail.com>
2022-05-10 12:18:41 +0200

    Added fetching

--- src/main/java/com/dubajandrzej/coronovirus/Tracker/service/CoronaVirusDataService.java
+++ src/main/java/com/dubajandrzej/coronovirus/Tracker/service/CoronaVirusDataService.java
@@ -1,9 +1,12 @@
 package com.dubajandrzej.coronovirus.Tracker.service;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -14,10 +17,16 @@ public class CoronaVirusDataService {
     private static String VIRUS_DATA_STRING = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/who_covid_19_situation_reports/who_covid_19_sit_rep_time_series/who_covid_19_sit_rep_time_series.csv";
 
     @PostConstruct
-    public void fetchVirusData() {
+    public void fetchVirusData() throws IOException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = (HttpRequest) HttpRequest.newBuilder().uri(URI.create(VIRUS_DATA_STRING)).build();
         HttpResponse<String> httpResponse = null;
+        try {
+            httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
         try {
             httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
         } catch (IOException e) {
@@ -25,7 +34,12 @@ public class CoronaVirusDataService {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        System.out.println(httpResponse.body());
+
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            String state = record.get("Province/States");
+            System.out.println(state);
+        }
     }
 }
 
