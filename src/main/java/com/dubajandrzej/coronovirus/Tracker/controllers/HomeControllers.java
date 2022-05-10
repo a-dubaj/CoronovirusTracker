370d86ce0b4d994a13f02fecc5a5e03620d79694
a-dubaj <dubaj.andrzej@gmail.com>
2022-05-10 14:10:47 +0200

    Location state and cases dispayed

--- src/main/java/com/dubajandrzej/coronovirus/Tracker/controllers/HomeControllers.java
+++ src/main/java/com/dubajandrzej/coronovirus/Tracker/controllers/HomeControllers.java
@@ -1,13 +1,20 @@
 package com.dubajandrzej.coronovirus.Tracker.controllers;
 
+import com.dubajandrzej.coronovirus.Tracker.service.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeControllers {
 
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("locationStats", coronaVirusDataService.getAllStats());
         return "home";
     }
 }
