aceb830ba713525a091f48848385abf7a5aaffd7
a-dubaj <dubaj.andrzej@gmail.com>
2022-05-10 14:50:32 +0200

    Update

--- src/main/java/com/dubajandrzej/coronovirus/Tracker/controllers/HomeControllers.java
+++ src/main/java/com/dubajandrzej/coronovirus/Tracker/controllers/HomeControllers.java
@@ -1,10 +1,13 @@
 package com.dubajandrzej.coronovirus.Tracker.controllers;
 
+import com.dubajandrzej.coronovirus.Tracker.models.LocationStats;
 import com.dubajandrzej.coronovirus.Tracker.service.CoronaVirusDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeControllers {
@@ -14,7 +17,11 @@ public class HomeControllers {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("locationStats", coronaVirusDataService.getAllStats());
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+
         return "home";
     }
 }
